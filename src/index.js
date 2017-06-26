a73511549905627893003f12b04c4902da791d3f
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-06-26 17:19:49 -0400

    movies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// Code goes here
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img
+          src={this.props.poster}
+          alt={this.props.title}
+        />
+        <h2>{this.props.title}</h2>
+        <small>Genres: {this.props.genres.join(', ')}</small>
+      </div>
+    );
+  }
+}
